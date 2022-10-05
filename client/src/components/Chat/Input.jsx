347f8d3cdd3ea5f1cd23922314848c44a443f5d9
Minal-singh <rishabhthakur15102@gmail.com>
2022-10-05 23:24:18 +0530

    added voice to text

--- client/src/components/Chat/Input.jsx
+++ client/src/components/Chat/Input.jsx
@@ -1,44 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, TextField } from "@mui/material";
-import '../../index.css'
+import "../../index.css";
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition
+const mic = new SpeechRecognition()
+
+mic.continuous = true
+mic.interimResults = true
+mic.lang = 'en-US';
+
 const Input = ({ setMessage, sendMessage, message }) => {
+  const [isListening, setIsListening] = useState(false)
+
+  useEffect(() => {
+    handleListen()
+    //eslint-disable-next-line
+  }, [isListening])
+
+  const handleListen = () => {
+    if (isListening) {
+      mic.start()
+      mic.onend = () => {
+        console.log('continue..')
+        mic.start()
+      }
+    } else {
+      mic.stop()
+      mic.onend = () => {
+        console.log('Stopped Mic on Click')
+      }
+    }
+    mic.onstart = () => {
+      console.log('Mics on')
+    }
+
+    mic.onresult = event => {
+      const transcript = Array.from(event.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+      console.log(transcript)
+      setMessage(transcript)
+      mic.onerror = event => {
+        console.log(event.error)
+      }
+    }
+  }
+
+
   return (
-    // <div style={{ padding:"10px",height: "20%" }}>
-    //   <form>
-    //     <TextField
-    //       id="message"
-    //       variant="outlined"
-    //       label="message"
-    //       placeholder="Type a message..."
-    //       value={message}
-    //       onChange={(e) => setMessage(e.target.value)}
-    //       onKeyPress={(event) =>
-    //         event.key === "Enter" ? sendMessage(event) : null
-    //       }
-    //       size="small"
-    //     />
-    //     <Button variant="contained" onClick={(e) => sendMessage(e)}>Send</Button>
-    //   </form>
-    // </div>
-
-
-<div className="foot form-group d-flex">
-<input id="message" 
-          placeholder="Type a message..."
-          value={message}
-          className="form-control bg-light"
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyPress={(event) =>
-            event.key === "Enter" ? sendMessage(event) : null
-          }
-/>
-<button onClick={(e) => sendMessage(e)} className="btn btn-primary mx-2"  >
-   
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
-            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"></path>
+    <div className="foot form-group d-flex">
+      <input
+        id="message"
+        placeholder="Type a message..."
+        value={message}
+        className="form-control bg-light"
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyPress={(event) =>
+          event.key === "Enter" ? sendMessage(event) : null
+        }
+      />
+      <button
+        onClick={() => setIsListening((prevState) => !prevState)}
+        className="btn btn-primary mx-2"
+      >
+        {isListening ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-mic-mute"
+            viewBox="0 0 16 16"
+          >
+            <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4.02 4.02 0 0 0 12 8V7a.5.5 0 0 1 1 0v1zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a4.973 4.973 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4zm3-9v4.879l-1-1V3a2 2 0 0 0-3.997-.118l-.845-.845A3.001 3.001 0 0 1 11 3z" />
+            <path d="m9.486 10.607-.748-.748A2 2 0 0 1 6 8v-.878l-1-1V8a3 3 0 0 0 4.486 2.607zm-7.84-9.253 12 12 .708-.708-12-12-.708.708z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-mic"
+            viewBox="0 0 16 16"
+          >
+            <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z" />
+            <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0v5zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3z" />
+          </svg>
+        )}
+      </button>
+      <button onClick={(e) => sendMessage(e)} className="btn btn-primary mx-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-send"
+          viewBox="0 0 16 16"
+        >
+          <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"></path>
         </svg>
-                        </button>   
-</div>
+      </button>
+    </div>
   );
 };
 
